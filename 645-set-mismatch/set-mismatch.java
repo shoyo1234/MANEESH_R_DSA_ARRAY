2adba62df5018bf704aaad690072bf4be5f87a72
MANEESH_R_ADS <136969721+shoyo1234@users.noreply.github.com>
2026-02-09 18:51:41 +0530

    Time: 15 ms (23.87%) | Memory: 48.7 MB (5.48%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+
+        int duplicate = -1;
+        int missing = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                duplicate = nums[i];
+            }
+            if (nums[i] > nums[i - 1] + 1) {
+                missing = nums[i - 1] + 1;
+            }
+        }
+
+        // If the missing number is n
+        if (nums[nums.length - 1] != nums.length) {
+            missing = nums.length;
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
