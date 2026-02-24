52303563110372f71d78cd6f1bce1f419f5db97b
MANEESH_R <136969721+shoyo1234@users.noreply.github.com>
2026-02-24 19:01:31 +0530

    Time: 15 ms (53.91%) | Memory: 49 MB (11.00%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -6,14 +6,14 @@ class Solution {
         // We traverse the array twice (from 2*size - 1 to 0)
         // Behaves like a circular array
         for(int i = 2*size-1; i >= 0; i--){
-
-            int current = nums[i%size];
+            int index = i % size;
+            int current = nums[index];
             while(stack.size() > 0 && current >= stack.peek()){
                 stack.pop();
             }
             //Only store elements in second pass as the index lies in the correct range (original array)
             if(i < size){
-                result[i] = stack.isEmpty() ? -1 : stack.peek();
+                result[index] = stack.isEmpty() ? -1 : stack.peek();
             }
             stack.push(current);
         }
