ee2ff67f7cd0527aa4318bf721db442e0b75038b
MANEESH_R <136969721+shoyo1234@users.noreply.github.com>
2026-02-24 18:57:44 +0530

    Time: 15 ms (53.91%) | Memory: 48.7 MB (31.39%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums){
+        int size = nums.length;
+        int result[] = new int[size];
+        Stack<Integer> stack = new Stack<>();
+        // We traverse the array twice (from 2*size - 1 to 0)
+        // Behaves like a circular array
+        for(int i = 2*size-1; i >= 0; i--){
+
+            int current = nums[i%size];
+            while(stack.size() > 0 && current >= stack.peek()){
+                stack.pop();
+            }
+            //Only store elements in second pass as the index lies in the correct range (original array)
+            if(i < size){
+                result[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(current);
+        }
+        return result;
+    }
+}
