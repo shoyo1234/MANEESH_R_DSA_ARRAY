71d4084604412740745dc0d3947bdb5be20aae24
MANEESH_R <136969721+shoyo1234@users.noreply.github.com>
2026-03-23 21:36:15 +0530

    Time: 77 ms (20.96%) | Memory: 106.7 MB (67.28%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int[] result_arr = new int[length];
+        // Stack to store index of elements
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = length-1; i>=0; i--){
+            // remove lesser or equal days
+            while(!stack.isEmpty() && 
+            temperatures[i] >= temperatures[stack.peek()]){
+                stack.pop();
+            }
+            if(!stack.isEmpty()){
+                result_arr[i] = stack.peek()-i;
+            }
+            // current days index stored
+            stack.push(i);
+        }
+        return result_arr;
+        
+    }
+}
